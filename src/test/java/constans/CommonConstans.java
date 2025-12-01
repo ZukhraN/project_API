12bd28cc30cf6eb3ffc23d4bc33b6030789a3533
znurmanova@dar.llp <zukhra.nurmanova@gmail.com>
2025-12-01 16:21:57 +0500

    Tests for pet api

--- src/test/java/constans/CommonConstans.java
+++ src/test/java/constans/CommonConstans.java
@@ -1,5 +1,10 @@
 package constans;
 
+import models.Pet;
+
 public class CommonConstans {
     public static final String BASE_URL = "https://petstore.swagger.io/v2/";
+
+    public static final Pet DEFAULT_PET = new Pet(0, "pet", "available");
+    public static final Pet UPDATED_PET = new Pet(1, "cat", "sold");
 }
---
+++ src/test/java/models/Pet.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Pet {
+    private long id;
+    private String name;
+    private String status;
+}
